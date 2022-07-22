0abec64db5d2ed739719ef2bd2aa083b1a20bce7
MorenaNobre <morenagnobre@gmail.com>
2022-07-22 10:58:42 -0300

    TypeScript

---
+++ typescript/index.ts
@@ -0,0 +1,90 @@
+// //acesso a types -> tipos, estruturas de dados para os parâmetros da sua classe.
+// //acesso a interfaces -> contrato para implementação das classes.
+
+// // //Exemplo Interface
+// interface IAnimal {
+//   name: string;
+//   type: "terrestre" | "aquatico";
+//   domestico: boolean;
+//   // executarRugido(alturaEmDecibeis: number): void;
+// }
+
+// //pode extender a interface
+// interface IFelino extends IAnimal {
+//   visaoNotura: boolean,
+// }
+
+// //exemplo type
+// interface ICanino extends IAnimal {
+//   porte: "pequeno" | "medio" | "grande"
+// }
+
+// type IDomestico = IFelino | ICanino;
+
+// const animal: IDomestico = {
+//   name: "Cachorro",
+//   type: "terrestre",
+//   domestico: false,
+//   porte: "pequeno",
+//   // executarRugido: (alturaEmDecibeis) => (`${alturaEmDecibeis}db`),
+// }
+
+// // animal.executarRugido(1000)
+// // console.log(animal)
+
+// const felino: IFelino = {
+//   name: "Gato",
+//   type: "terrestre",
+//   visaoNotura: true,
+//   domestico: true,
+//   // executarRugido: (alturaEmDecibeis) => (`${alturaEmDecibeis}db`),
+// }
+
+// // console.log(felino)
+
+// //==============================================================
+
+// // function soma(a: number, b: number) {
+// //   return a + b;
+// // }
+
+// // console.log(soma(13, 10))
+
+// const input = document.getElementById("input") as HTMLInputElement;
+
+// //tratamento de input
+
+// input.addEventListener("input", (event) => {
+//   const i = event.currentTarget as HTMLInputElement;
+// })
+
+function adicionaApendiceALista<T>(array: T[], valor: T) {
+  //generic types
+  return array.map(() => valor);
+}
+
+//pede que sejam todos os tipos de dados iguais
+adicionaApendiceALista(["A", "B", "C"], "d");
+
+
+//Desenvolvendo Condicionais a partir de parâmetros
+interface IUsuario {
+  id: string;
+  email: string;
+  //Variáveis opcionais com caracter "?"
+  cargo?: "funcionario" | "gerente" | "coordenador" | "supervisor",
+}
+
+// interface IAdmin extends IUsuario {
+//   cargo: "gerente" | "coordenador" | "supervisor",
+// }
+
+function redirecione(usuario: IUsuario) {
+  if(usuario.cargo) {
+    //redirecionar(usuario.cargo);
+  }
+
+  //redirecionar para área do usuario
+}
+
+
