6dee88bfaa6c93f72ea33b15de78b2b6be26fde2
MorenaNobre <morenagnobre@gmail.com>
2022-07-22 11:14:47 -0300

    TypeScript readoly and private

--- typescript/index.ts
+++ typescript/index.ts
@@ -87,4 +87,30 @@ function redirecione(usuario: IUsuario) {
   //redirecionar para área do usuario
 }
 
+//Readonly e private
+
+interface Cachorro {
+  nome: string;
+  idade: number;
+  parqueFavorito?: string;
+}
+
+type CachorroSomenteLeitura = {
+  +readonly [K in keyof Cachorro]-?: Cachorro[K];
+}
+
+class MeuCachorro implements CachorroSomenteLeitura {
+  idade;
+  nome;
+  parqueFavorito;
+
+  constructor(nome, idade) {
+    this.nome = nome;
+    this.idade = idade;
+  }
+}
+
+const cao = new MeuCachorro("Bidu", 8);
+
+
 
