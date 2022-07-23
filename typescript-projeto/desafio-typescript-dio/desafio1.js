5259c35ad1472209bdef667898ffb72962dadc18
MorenaNobre <morenagnobre@gmail.com>
2022-07-23 14:03:55 -0300

    Typescript - Challenge 1

---
+++ typescript-projeto/desafio-typescript-dio/desafio1.js
@@ -0,0 +1,26 @@
+"use strict";
+//Como podemos rodar o código abaixo em um arquivo .ts sem causar erros?
+const tieta = {
+    codigo: 42,
+    nome: "Tieta do Agreste",
+};
+const tonho = {
+    codigo: 43,
+    nome: "Tonho da Lua",
+};
+function funcionarios(codigo, nome) {
+    console.log(`O código de ${nome} é: ${codigo}`);
+}
+funcionarios(tieta.codigo, tieta.nome);
+funcionarios(tonho.codigo, tonho.nome);
+// == Resposta2 == //
+const objFuncionario = {};
+objFuncionario.codigo = 44;
+objFuncionario.nome = "Xuxa Meneguel";
+console.log(objFuncionario);
+// == Resposta3 == //
+const funcionario2 = {
+    codigo: 10,
+    nome: "Silvio Santos",
+};
+console.log(funcionario2);
---
+++ typescript-projeto/desafio-typescript-dio/desafio1.ts
@@ -0,0 +1,49 @@
+//Como podemos rodar o código abaixo em um arquivo .ts sem causar erros?
+
+/** 
+let funcionario = {};
+funcionario.codigo = 10;
+funcionario.nome = "John Doe";
+*/
+
+// == RESPOSTAS == //
+
+// == Resposta1 == //
+interface Funcionario {
+  codigo: number;
+  nome: string;
+}
+
+const tieta: Funcionario = {
+  codigo: 42,
+  nome: "Tieta do Agreste",
+};
+
+const tonho: Funcionario = {
+  codigo: 43,
+  nome: "Tonho da Lua",
+};
+
+function funcionarios(codigo: number, nome: string) {
+  console.log(`O código de ${nome} é: ${codigo}`);
+}
+
+funcionarios(tieta.codigo, tieta.nome)
+funcionarios(tonho.codigo, tonho.nome)
+
+
+// == Resposta2 == //
+const objFuncionario = {} as Funcionario;
+objFuncionario.codigo = 44;
+objFuncionario.nome = "Xuxa Meneguel"
+
+console.log(objFuncionario);
+
+
+// == Resposta3 == //
+const funcionario2: {codigo:number, nome:string} = {
+  codigo: 10,
+  nome: "Silvio Santos",
+}
+
+console.log(funcionario2)
