b6d6e225acf2baac1d89405bfbd2473f65607ec0
MorenaNobre <morenagnobre@gmail.com>
2022-07-28 11:59:01 -0300

    Angular - base structure

--- angular-ambiente/src/app/app.component.ts
+++ angular-ambiente/src/app/app.component.ts
@@ -1,4 +1,3 @@
-import { PeopleService } from './shared/services/people.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -8,39 +7,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AppComponent implements OnInit {
   count = 0;
-  nome = 'Ivonaldo Junior Escobar Soares';
-  text = '';
 
-  pessoas = [
-    {
-      firstName: '',
-      lastName: '',
-      age: 0
-    },
-  ];
-
-  constructor(private peopleService: PeopleService) {
+  constructor() {
 
   }
 
   ngOnInit() {
-    this.getPeople();
-    let interval = setInterval(() => {
-      this.count++;
-      if(this.count === 10) {
-        clearInterval(interval);
-      }
-    }, 1000)
+    
   }
-
-  clicou(nome: string): void {
-    console.log('Clicou em min', nome)
-  }
-
-  getPeople() {
-    this.peopleService.getPeople().subscribe(people => {
-      this.pessoas = people;
-    })
-  }
-
+  
 }
