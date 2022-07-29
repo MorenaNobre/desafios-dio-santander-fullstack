a84e3d750127e3d8cda615630ea2cfb4f066c5a7
MorenaNobre <morenagnobre@gmail.com>
2022-07-29 16:06:30 -0300

    Angular-CRUD - Users List Component

--- angular-crud/src/app/app.module.ts
+++ angular-crud/src/app/app.module.ts
@@ -4,13 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
-import { UsersComponent } from './pages/users/users.component';
+import { UsersListComponent } from './pages/users/users-list/users-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    UsersComponent
+    UsersListComponent
   ],
   imports: [
     BrowserModule,
