2cc331c4e355581eafba58d6a9107a86fcd3e24f
MorenaNobre <morenagnobre@gmail.com>
2022-07-29 12:02:34 -0300

    Angular - HttpClientModule | bring results from API

--- angular-ambiente/src/app/app.module.ts
+++ angular-ambiente/src/app/app.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { MarcaTextoDirective } from './directives/marca-texto.directive';
 import { PeopleService } from './shared/services/people.service';
-import { ListComponent } from './shared/components/list/list.component';
 import { ListApiComponent } from './components/list-api/list-api.component';
+import { ListComponent } from './shared/components/list/list.component';
 
 @NgModule({
   declarations: [
@@ -15,11 +16,8 @@ import { ListApiComponent } from './components/list-api/list-api.component';
     ListComponent,
     ListApiComponent,
   ],
-  imports: [
-    BrowserModule,
-    FormsModule,
-  ],
+  imports: [BrowserModule, FormsModule, HttpClientModule],
   providers: [PeopleService],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
