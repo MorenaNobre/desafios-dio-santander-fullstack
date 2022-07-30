39f14b60833ce82f9fa40d34f986e57c3f28b697
MorenaNobre <morenagnobre@gmail.com>
2022-07-30 04:12:15 -0300

    Angular-CRUD - post method using sheet best api and google sheets

--- angular-crud/src/app/services/user.service.ts
+++ angular-crud/src/app/services/user.service.ts
@@ -1,20 +1,30 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { User } from '../models/user';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class UserService {
-  apiUrl = 'https://sheet.best/api/sheets/ec1db859-b6ac-4752-814b-441ad468e9c4'
+  apiUrl = 'https://sheet.best/api/sheets/4e164f1e-d93e-4599-bdb7-63d222c4bc9a';
+  httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type': 'application/json',
+    }),
+  };
 
-  constructor(private httpClient: HttpClient) { }
+  constructor(private httpClient: HttpClient) {}
 
   //C.R.U.D. -> CREATE, READ, UPDATE, DELETE
 
   //Retorna lista de usuarios READ
-  getUsers():Observable<User[]> {
+  getUsers(): Observable<User[]> {
     return this.httpClient.get<User[]>(this.apiUrl);
+  }
+
+  //Salva usuário no banco CREATE
+  postUser(user: User): Observable<User> {
+    return this.httpClient.post<User>(this.apiUrl, user, this.httpOptions);
   }
 }
