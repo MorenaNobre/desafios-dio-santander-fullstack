ebab4ca368e180d47d26b828805d140addd124d8
MorenaNobre <morenagnobre@gmail.com>
2022-07-31 19:47:33 -0300

    Angular-Todo List - LocalStorage

--- angular-crud/src/app/pages/todo/todo-item/todo-item.component.ts
+++ angular-crud/src/app/pages/todo/todo-item/todo-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { Todo } from 'src/app/models/todo';
 
 @Component({
@@ -12,6 +12,9 @@ export class TodoItemComponent implements OnInit {
     title: '',
     done: false,
   };
+  @Output() remove = new EventEmitter;
+
+  done = false;
 
   constructor() { 
   }
@@ -20,11 +23,11 @@ export class TodoItemComponent implements OnInit {
   }
 
   removeTodo(): void {
-    console.log(this.todo)
+    this.remove.emit(this.todo);
   }
 
   markAsDone(): void{
-    console.log(this.todo)
+    this.done = true;
   }
 
 }
--- angular-crud/src/app/pages/todo/todo-list/todo-list.component.ts
+++ angular-crud/src/app/pages/todo/todo-list/todo-list.component.ts
@@ -16,12 +16,30 @@ export class TodoListComponent implements OnInit {
 
   constructor() {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    let items: any = localStorage.getItem('todos');
+    let todos = JSON.parse(items);
+    if (!todos) {
+      this.todos = [];
+    } else {
+      this.todos = todos;
+    }
+  }
 
   addTodo(title: string) {
-    this.todo.id = this.todos.length + 1;
-    this.todo.title = title;
-    this.todos.push(this.todo);
-    console.log(this.todos);
+    const id = this.todos.length + 1;
+    this.todos.push({
+      id: id,
+      title: title,
+      done: false,
+    });
+    localStorage.setItem('todos', JSON.stringify(this.todos));
+  }
+
+  removeTodo(todo: any) {
+    console.log('O elemento pai recebeu', todo);
+    let index = this.todos.indexOf(todo);
+    this.todos.splice(index, 1);
+    localStorage.setItem('todos', JSON.stringify(this.todos));
   }
 }
