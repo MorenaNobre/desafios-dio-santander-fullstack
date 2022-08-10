eff11cd885393c44e04518f51cd1c3f12a0c5374
MorenaNobre <morenagnobre@gmail.com>
2022-08-10 14:48:57 -0300

    Desafio Banco - Herança

--- java-pratica/DesafioDoBanco/src/Banco/ContaPoupanca.java
+++ java-pratica/DesafioDoBanco/src/Banco/ContaPoupanca.java
@@ -1,21 +1,7 @@
 package Banco;
 
-public class ContaPoupanca {
+public class ContaPoupanca extends Conta{
 
-    private  int agencia;
-    private int numero;
-    private double saldo;
 
-    public void sacar() {
-
-    }
-
-    public void depositar() {
-
-    }
-
-    public void transferir() {
-
-    }
 
 }
